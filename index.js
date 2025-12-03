c2ad36dd83bec9f4f8516f3808b5fa1086fbd63b
hussein-1012 <hm3186@fayoum.edu.eg>
2025-12-03 22:06:26 +0200

    Finish task

--- index.js
+++ index.js
@@ -1,71 +1,80 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const app = express();
+require('dotenv').config();
 
+// إعدادات الـ Express
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// Private App Token
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_API_KEY;
+const OBJECT_ID = '2-195413524';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// 🏠 ROUTE 1 - Homepage: عرض كل Custom Objects
+app.get('/', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_ID}?properties=name&properties=type&properties=avg_price`;
 
-// * Code for Route 1 goes here
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+  try {
+    const response = await axios.get(url, { headers });
+    const data = response.data.results.map(obj => ({
+      id: obj.id,
+      name: obj.properties.name,
+      type: obj.properties.type,
+      avg_price: obj.properties.avg_price
+    }));
 
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    res.render('homepage', {
+      title: 'My Custom Objects | HubSpot',
+      data
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send('Error fetching data');
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// 📝 ROUTE 2 - صفحة الفورم لإضافة Custom Object
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Add Custom Object | HubSpot'
+  });
 });
-*/
 
+// 🚀 ROUTE 3 - POST لإنشاء أو تحديث Custom Object
+app.post('/update-cobj', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_ID}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  const newObj = {
+    properties: {
+      name: req.body.name,
+      type: req.body.type === 'true', // تحويل Boolean لو Property من نوع Boolean
+      avg_price: req.body.avg_price
+    }
+  };
+
+  try {
+    await axios.post(url, newObj, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send('Error adding new record');
+  }
+});
+
+// 🚀 تشغيل السيرفر
+const PORT = 3000;
+app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
